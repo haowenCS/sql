62abc369e108c2b070008b2ca033790d1a7eb2ee
gaotianpeng <gtpsydm@gmail.com>
2021-07-11 00:02:34 +0800

    test for research

--- postgres/table_test.sql
+++ postgres/table_test.sql
@@ -1,16 +1,44 @@
--- table with partition
-CREATE TABLE measurement(
-  city_id int not null,
-  logdate date primary key,
-  peaktemp int,
-  unitsales int
-) PARTITION BY RANGE(logdate);
+create table grade (
+  gno varchar(20) primary key
+);
 
-select tablename from pg_tables where schemaname='public';
-DROP TABLE measurement;
+create table class (
+    classno varchar(20) primary key ,
+    classname varchar(30),
+    gno varchar(20),
+    foreign key (gno) references grade(gno)
+);
 
-CREATE TABLE measurement_y2006m02 PARTITION OF measurement
-    FOR VALUES FROM ('2006-02-01') TO ('2006-03-01');
+create table student (
+    sno varchar(20) primary key,
+    sname varchar(30),
+    sex varchar(5),
+    age int,
+    nation varchar(20),
+    classno varchar(20)
+);
 
-CREATE TABLE measurement_y2006m03 PARTITION OF measurement
-    FOR VALUES FROM ('2006-03-01') TO ('2006-04-01');
+create table course (
+    cno varchar(20) primary key,
+    cname varchar(30),
+    credit int,
+    priorcourse varchar(20),
+    foreign key (priorcourse) references course(cno)
+);
+
+create table sc (
+    sno varchar(20),
+    cno varchar(20),
+    score int,
+    primary key (sno, cno),
+    foreign key (sno) references student(sno),
+    foreign key (cno) references course(cno)
+);
+
+select classno, classname, AVG(score) as avg_score
+from sc, (SELECT * FROM class WHERE class.gno = '2005') as sub
+where sc.sno IN (select sno from student where student.classno = sub.classno) and
+sc.cno IN(select course.cno from course where course.cname = '高等数据')
+group by classno, classname
+having avg(score) > 80.0
+order by avg_score;
